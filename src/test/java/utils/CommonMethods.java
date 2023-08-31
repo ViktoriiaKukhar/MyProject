4b2da460ae13c886168f733cd5acdd52fca2c31e
ViktoriiaKukhar <viktoriya.kukhar@gmail.com>
2023-08-31 17:21:39 +0300

    First Commit

---
+++ src/test/java/utils/CommonMethods.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+public class CommonMethods {
+
+    public static WebDriver driver;
+
+    public static void openBrowser() {
+
+    }
+
+}
+
